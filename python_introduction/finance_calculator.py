7e641ed90c027af81be69394e0c58a9f9b1bd119
Rozthegray <rozthegrey@gmail.com>
2024-12-22 11:10:56 -0800

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-monthly_income = float(input("Enter Your Monthly Income"))
+monthly_income = float(input("Enter your monthly income: "))
 
-monthly_expenses = float(input("Enter your monthly expenses"))
+monthly_expenses = float(input("Enter your monthly expenses: "))
 
 monthly_savings = monthly_income - monthly_expenses
 
